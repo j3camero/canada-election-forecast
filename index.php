f241f44451e6ac00c1e16c0c7d95350b5d20f3c1
Jeff Cameron <cameron.jp@gmail.com>
2015-10-12 21:48:23 -0400

    Data update for Oct 12. Also added a maple leaf favicon.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once 'barchart.php';
   <meta charset="ISO-8859-1">
   <!-- <meta name=viewport content="width=device-width, initial-scale=1"> -->
   <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="shortcut icon" type="image/x-icon" href="mapleleaf.ico">
 </head>
 <body>
 <script>
